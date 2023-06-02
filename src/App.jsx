dc3395e50e5d4aff3bb2bbd5e734892ee4d02263
Mauri-salazar <maurisa2004@gmail.com>
2023-06-02 01:27:12 -0300

    create vite app and installation the tailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+import './App.css'
+
+function App() {
+    return (
+    <Fragment>
+      <div className='bg-black'>
+        Hello world
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
