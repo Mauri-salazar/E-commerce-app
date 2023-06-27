337bf7073da2927bd82c180d507305e4af0e3582
Mauri-salazar <salazar.mauricio.dev@gmail.com>
2023-06-27 00:41:43 -0300

    add OrderCard Component container card the my order

--- src/Components/OrderCard/OrderCard.jsx
+++ src/Components/OrderCard/OrderCard.jsx
@@ -1,39 +1,26 @@
-// import { ShoppingCartContext } from "../Context/Context";
-// import { useContext } from "react";
-import { AiFillDelete } from "@react-icons/all-files/ai/AiFillDelete";
-import PropTypes  from "prop-types";
+import { XMarkIcon } from '@heroicons/react/24/solid';
 
-
-export const OrderCard = props => {
-  const {id, title, image, price, handleDelete} = props;
+export const OrderCard = (props) => {
+  const { id, title, imageUrl, price, handleDelete } = props;
+  
+  let renderXMarkIcon
+  if (handleDelete) {
+    renderXMarkIcon = <XMarkIcon onClick={() => handleDelete(id)} className='h-6 w-6 text-black cursor-pointer'></XMarkIcon>
+  }
 
   return (
     <div className="flex justify-between items-center mb-3">
-      <div className="flex items-center grap-2">
-        <figure className="w-20 h-20">
-          <img
-            className="w-full h-full rounded-lg object-cover"
-            src={image}
-            alt={title}
-          />
+      <div className='flex items-center gap-2'>
+        <figure className='w-20 h-20'>
+          <img className='w-full h-full rounded-lg object-cover' src={imageUrl} alt={title} />
         </figure>
-        <p className="text-sm font-light">{title}</p>
+        <p className='text-sm font-light'>{title}</p>
       </div>
-      <div className="flex items-center grap-2">
-        <p className="text-lg font-medium">{price}</p>
-        <AiFillDelete
-          className="w-6 h-6 text-red-500 cursor-pointer"
-          onClick={() => handleDelete(id)}
-        />
+      <div className='flex items-center gap-2'>
+        <p className='text-lg font-medium'>{price}</p>
+        {renderXMarkIcon}
       </div>
     </div>
-  );
+  )
 };
 
-OrderCard.propTypes = {
-  title: PropTypes.node.isRequired,
-  image: PropTypes.node.isRequired,
-  price: PropTypes.node.isRequired,
-  handleDelete: PropTypes.func,
-  id: PropTypes.node.isRequired
-}
--- src/Pages/MyOrder/MyOrder.jsx
+++ src/Pages/MyOrder/MyOrder.jsx
@@ -1,9 +1,27 @@
-import { Layout } from "../../Components/Layout/Layout";
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../Context'
+import Layout from '../../Components/Layout/Layout'
+import OrderCard from '../../Components/OrderCard'
 
 export const MyOrder = () => {
+  const context = useContext(ShoppingCartContext)
+
   return (
-    <Layout className="bg-red-100">
+    <Layout>
       MyOrder
+      <div className='flex flex-col w-80'>
+        {
+          context.order?.slice(-1)[0].products.map(product => (
+            <OrderCard
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              imageUrl={product.images}
+              price={product.price}
+            />
+          ))
+        }
+      </div>
     </Layout>
-  );
+  )
 };
